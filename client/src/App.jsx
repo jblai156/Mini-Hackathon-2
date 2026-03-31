f2ef5e0886baad287caa0b8cef9b3b373aef16b5
Sidney Nguyen <sydneynguyen05@yahoo.ca>
2026-03-30 19:45:07 -0600

    basic card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const fetcher = url => axios.get(url).then(res => res.data);
 
 function App() {
+  const title = 'Kanban Collaboration Board'
+  const id = "BUS-1"
+  const points =5
   return (
-    <div className="display-1 text-info">
-      Hello
+    <div className="border rounded px-2 m-2 bg-base text-black">
+    <div className="display-1 fw-semibold">
+      {title}
+    </div>
+    <div className="display-3 d-flex gap-4 justify-content-between py-2 text-secondary fw-normal">
+      <div>{id}</div>
+      <div>{points}</div>
+    </div>
     </div>
   )
 }
