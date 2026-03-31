a11e35775969fe247173af7aac8248649937edea
Sidney Nguyen <sydneynguyen05@yahoo.ca>
2026-03-30 19:07:34 -0600

    installed bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const fetcher = url => axios.get(url).then(res => res.data);
 
 function App() {
   return (
-    <div>
+    <div className="display-1 text-info">
       Hello
     </div>
   )
