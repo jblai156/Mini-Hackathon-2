d5b8d6fe0aa5abacc8b083344e596b88bd741a33
Sidney Nguyen <sydneynguyen05@yahoo.ca>
2026-03-30 22:50:25 -0600

    types

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,24 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import TaskCard from './task-card'
+import TaskCard from './component/task-card'
 
 const fetcher = url => axios.get(url).then(res => res.data);
 
 function App() {
-  const title = 'Kanban Collaboration Board'
-  const id = "BUS-1"
-  const points =5
+  const task = {
+    title: 'Do Market Research',
+    id: 'BUS-1',
+    points: 5
+  }
 
   return (
     <>
-    <TaskCard title={title} id={id} point={points} />
+    <TaskCard task={task} />
+
+    {/* <TaskCard title={title} id={id} point={points} />
     <TaskCard title='Competitor analysis' id='BUS-2' points={3}/>
     <TaskCard title='Develop Business Strategy' id='BUS-3' points={8}/>
-    <TaskCard title='Develop Marketing Strategy' id='BUS-4' points={5}/>
-
+    <TaskCard title='Develop Marketing Strategy' id='BUS-4' points={5}/> */}
 </>
   )
 }
