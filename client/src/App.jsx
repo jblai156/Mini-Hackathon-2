6ad3b71df944368d518c39b30bd05c50a94e164e
Sidney Nguyen <sydneynguyen05@yahoo.ca>
2026-04-06 02:44:23 -0600

    hover effects

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,14 +9,10 @@ function App() {
   const columns = statuses.map((status) => {
     const tasksInColumn = tasks.filter((task) => task.status === status)
     return {
-      title: status,
+      status,
       tasks: tasksInColumn
     }
   })
-
-  const todoTasks = tasks.filter((task) => task.status === 'todo')
-  const inProgressTasks = tasks.filter((task) => task.status === 'inProgress')
-  const doneTasks = tasks.filter((task) => task.status === 'done')
 
   const updateTask = (task) => {
     const updatedTasks = tasks.map((t) => {
@@ -27,6 +23,7 @@ function App() {
 
   const handleDrop = (e, status) => {
     e.preventDefault()
+    setCurrentlyHoveringOver(null)
     const id = e.dataTransfer.getData("id")
     const task = tasks.find((task) => task.id === id)
     if(task) {
@@ -34,25 +31,34 @@ function App() {
     }
   }
   
+  const [currentlyHoveringOver, setCurrentlyHoveringOver] = useState(null)
+  const handleDragEnter = (status) => {
+    console.log('drag enter', status)
+    setCurrentlyHoveringOver(status)
+  }
+
   return (
   <div className="container-fluid mt-4">
     <div className="row">
       {columns.map((column) => (
       <div
-      onDrop={(e) => handleDrop(e, column.title)}
-      onDragOver={(e) => e.preventDefault()} 
+      onDrop={(e) => handleDrop(e, column.status)}
+      onDragOver={(e) => e.preventDefault()}
+      onDragEnter={() => handleDragEnter(column.status)}
       className="col-md-3 border-end"
       >
           <h1 className="d-flex justify-content-between align-items-center mb-3 px-2 fw-bold text-secondary-emphasis">
-            <h1 className="text-capitalize  m-0">{column.title}</h1>
+            <h1 className="text-capitalize  m-0">{column.status}</h1>
             {column.tasks.reduce((total, task) => total + (task?.points || 0), 0)}
           </h1>
-          {column.tasks.map((task) => (
-            <TaskCard
-            task={task}
-            updateTask={updateTask}
-            />
-          ))}
+          <div className={`h-100 ${currentlyHoveringOver === column.status ? 'bg-secondary-subtle' : ''}`}>
+            {column.tasks.map((task) => (
+              <TaskCard
+              task={task}
+              updateTask={updateTask}
+              />
+            ))}
+          </div>
         </div>
       ))}
     </div>
